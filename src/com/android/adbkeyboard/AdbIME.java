01c657bd6fb953d86e649f9d443d5a660a1e5a4a
erict <senzhk@gmail.com>
2013-09-06 19:38:29 +0800

    first commit

---
+++ src/com/android/adbkeyboard/AdbIME.java
@@ -0,0 +1,79 @@
+package com.android.adbkeyboard;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.inputmethodservice.InputMethodService;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class AdbIME extends InputMethodService { 
+	
+    private String IME_MESSAGE = "ADB_INPUT_TEXT";
+    private String IME_KEYCODE = "ADB_INPUT_CODE";
+    private String IME_EDITORCODE = "ADB_EDITOR_CODE";
+    private BroadcastReceiver mReceiver = null;
+    
+    
+    @Override 
+    public View onCreateInputView() {
+    	
+    	View mInputView = 
+            (View) getLayoutInflater().inflate( R.layout.view, null);
+    
+        if (mReceiver == null) {
+        	IntentFilter filter = new IntentFilter(IME_MESSAGE);
+        	filter.addAction(IME_KEYCODE);
+        	filter.addAction(IME_EDITORCODE);
+        	mReceiver = new AdbReceiver();
+        	registerReceiver(mReceiver, filter);
+        }
+
+        return mInputView; 
+    } 
+    
+    
+    public void onDestroy() {
+    	if (mReceiver != null)
+    		unregisterReceiver(mReceiver);
+    	super.onDestroy();    	
+    }
+    
+    class AdbReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			
+			if (intent.getAction().equals(IME_MESSAGE)) {
+				String msg = intent.getStringExtra("msg");				
+				if (msg != null) {					
+					InputConnection ic = getCurrentInputConnection();
+					if (ic != null)
+						ic.commitText(msg, 1);
+				}
+			}
+			
+			if (intent.getAction().equals(IME_KEYCODE)) {				
+				int code = intent.getIntExtra("code", -1);				
+				if (code != -1) {
+					InputConnection ic = getCurrentInputConnection();
+					if (ic != null)
+						ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, code));
+				}
+			}
+			
+			if (intent.getAction().equals(IME_EDITORCODE)) {				
+				int code = intent.getIntExtra("code", -1);				
+				if (code != -1) {
+					InputConnection ic = getCurrentInputConnection();
+					if (ic != null)
+						ic.performEditorAction(code);
+				}
+			}
+			
+		}
+
+    }
+}
