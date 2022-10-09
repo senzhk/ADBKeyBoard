9c1c0c3d1a9c4848421355591ea0af778bc534a4
senzhk <senzhk@gmail.com>
2022-10-09 22:15:36 +0800

    Update AdbIME.java
    update comment.

--- keyboardservice/src/main/java/com/android/adbkeyboard/AdbIME.java
+++ keyboardservice/src/main/java/com/android/adbkeyboard/AdbIME.java
@@ -106,7 +106,7 @@ public class AdbIME extends InputMethodService {
 					for (i = 0; i < mcodes.length - 1; i = i + 2) {
 						if (ic != null) {
 							KeyEvent ke;
-							if (mcodes[i].contains("+")) { // Check metaState if more than one.
+							if (mcodes[i].contains("+")) { // Check metaState if more than one. Use '+' as delimiter
 								String[] arrCode = mcodes[i].split("\\+"); // Get metaState if more than one.
 								ke = new KeyEvent(
 										0,
